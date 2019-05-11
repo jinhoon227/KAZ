fe4c2b78dc1ef1a81a52bc9e77bfb88c81485c56
jinhoon <g22203@naver.com>
2019-05-11 17:50:17 +0900

    fixed #1 weather update

--- KAZ/app/src/main/java/samstnet/com/kaz/MainActivity.java
+++ KAZ/app/src/main/java/samstnet/com/kaz/MainActivity.java
@@ -1,12 +1,21 @@
 package samstnet.com.kaz;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import samstnet.com.kaz.gps.ConverterGridGps;
+import samstnet.com.kaz.gps.GpsInfo;
+import samstnet.com.kaz.gps.LatXLngY;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
     private Menu3FragWeather menu3FragWeather = new Menu3FragWeather();
     private Menu4FragConfig menu4FragConfig = new Menu4FragConfig();
 
+    //for Gps
+    private final int PERMISSIONS_ACCESS_FINE_LOCATION = 1000;
+    private final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1001;
+    private boolean isAccessFineLocation = false;
+    private boolean isAccessCoarseLocation = false;
+    private boolean isPermission = false;
+
+    public static int TO_GRID = 0;
+    private GpsInfo gps;
+    private LatXLngY grid;
+    private ConverterGridGps converterGridGps;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
         // 첫 화면 지정
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.frame_layout, menu1FragGrowth).commitAllowingStateLoss();
+
+        //파싱 쓰레드
+        //GetXMLTask task = new GetXMLTask();
 
         // bottomNavigationView의 아이템이 선택될 때 호출될 리스너 등록
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -55,5 +80,90 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        // 권한 요청을 해야 함
+        if (!isPermission) {
+            callPermission();
+            if(isPermission) {
+                UsingGps();
+            }
+        }
+        else {
+           UsingGps();
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_ACCESS_FINE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            isAccessFineLocation = true;
+
+        } else if (requestCode == PERMISSIONS_ACCESS_COARSE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            isAccessCoarseLocation = true;
+        }
+
+        if (isAccessFineLocation && isAccessCoarseLocation) {
+            isPermission = true;
+        }
+
+        //권한을 수락했다면 위치정보 가져오기
+        if(isAccessFineLocation){
+            UsingGps();
+        }
+    }
+
+    // 위치 권한 요청
+    private void callPermission() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_ACCESS_FINE_LOCATION);
+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSIONS_ACCESS_COARSE_LOCATION);
+        } else {
+            isPermission = true;
+        }
+    }
+
+    private void UsingGps(){
+        gps = new GpsInfo(MainActivity.this);
+        // GPS 사용유무 가져오기
+        if (gps.isGetLocation()) {
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+
+            converterGridGps = new ConverterGridGps();
+            grid = converterGridGps.getGridValue(TO_GRID, latitude, longitude);
+            Log.d("ddd","당신의 위치 - " + grid.x + "    " + grid.y);
+            Toast.makeText(
+                    getApplicationContext(),
+                    "당신의 위치 - " + grid.x + "    " + grid.y,
+                    Toast.LENGTH_LONG).show();
+            //task.execute("http://www.kma.go.kr/wid/queryDFS.jsp?gridx=" + grid.x + "&gridy=" + grid.y);
+
+        } else {
+            // GPS 를 사용할수 없으므로
+            gps.showSettingsAlert();
+        }
+    }
+
+
+
+
+
 }
