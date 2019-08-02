7af3ff1f8bec0f04e2401eb35cef793dca5f5deb
seogi9808 <seogi9898@gmail.com>
2019-08-03 02:21:07 +0900

    123

--- KAZ/app/src/main/java/samstnet/com/kaz/eventbus/Customer.java
+++ KAZ/app/src/main/java/samstnet/com/kaz/eventbus/Customer.java
@@ -87,11 +87,11 @@ public class Customer extends Application {
         String Intialize_itemwear;
 
         Initalize_plant = "1&0&PLANT1&1&5&50";
-        Intialize_item[0]= "물뿌리개&비를 피할 수 있는 우산을 씌워 줍니다&" + R.drawable.img1 + "&10&false&false";
+        Intialize_item[0]= "물뿌리개&비를 피할 수 있는 우산을 씌워 줍니다&" + R.drawable.sprinklerimg+ "&10&false&false";
         Intialize_item[1]="비료&식물이 더 잘 자랄 수 있는 영양분을 제공합니다&" + R.drawable.img2 + "&20&false&false";
         Intialize_item[2]="우산&비를 피할 수 있는 우산을 씌워 줍니다&" + R.drawable.img3 + "&50&false&false";
-        Intialize_item[3]="썬글라스&강한 햇빛을 피할 수 있는 썬글라쓰를 씌워 줍니다&" + R.drawable.img4 + "&60&false&false";
-        Intialize_item[4]="목도리&추위를 피할 수 있게 목도리를 두릅니다.&" + R.drawable.img4 + "&100&false&false";
+        Intialize_item[3]="썬글라스&강한 햇빛을 피할 수 있는 썬글라쓰를 씌워 줍니다&" + R.drawable.sunglassimg + "&60&false&false";
+        Intialize_item[4]="목도리&추위를 피할 수 있게 목도리를 두릅니다.&" + R.drawable.scarf + "&100&false&false";
         Intialize_setting= "false&false&false&false&false";
         Intialize_itemwear="false&false&false&false&false";
 
@@ -113,7 +113,7 @@ public class Customer extends Application {
             for(int i=0;i<5;i++) {
                 editor.putString("item"+i, Intialize_item[i]); //item1라는 key값으로 id 데이터를 저장한다.
             }
-            editor.putString("setting", "false&false&false&false&false");
+            editor.putString("setting", "false&false&false&false&true");
             editor.putString("itemwear", "false&false&false&false&false");
             editor.commit(); //완료한다.
         }
