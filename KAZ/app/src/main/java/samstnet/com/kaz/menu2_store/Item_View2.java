24fbb133e36787a0fbab6c9fb92cc0e4874f0c3f
seogi9808 <seogi9898@gmail.com>
2019-08-04 23:35:32 +0900

    dev

--- KAZ/app/src/main/java/samstnet/com/kaz/menu2_store/Item_View2.java
+++ KAZ/app/src/main/java/samstnet/com/kaz/menu2_store/Item_View2.java
@@ -1,5 +1,6 @@
 package samstnet.com.kaz.menu2_store;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import samstnet.com.kaz.R;
 public class Item_View2 extends LinearLayout {
     TextView textView2,textView,textView3;
     ImageView imageView;
+    LinearLayout LL;
     public Item_View2(Context context) {
         super(context);
         init(context);
@@ -24,11 +26,16 @@ public class Item_View2 extends LinearLayout {
     public void init(Context context) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.list_item,this ,true);
+        LL = (LinearLayout)findViewById(R.id.layout_listitem);
         textView = (TextView) findViewById(R.id.textView);
         textView2 = (TextView) findViewById(R.id.textView2);
         textView3 = (TextView) findViewById(R.id.textView_buy);
         imageView= (ImageView) findViewById(R.id.imageView);
 
+    }
+    @SuppressLint("ResourceAsColor")
+    public void background_change(){
+        LL.setBackgroundResource(R.drawable.shop_item_ui2);
     }
     public void setName(String name)
     {
